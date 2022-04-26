74a69d654c56be7ac460d121eb19c4b28b61e3ba
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-04-25 21:18:20 -0400

    Create header component, event handlers and delete button

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,31 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import TodosList from "./TodosList";
+import Header from "./Header";
 
-class TodoContainer extends React.Component {
+class TodoContainer extends Component {
   state = {
     todos: [
       {
         id: 1,
         title: "Setup development environment",
-        completed: true
+        completed: true,
       },
       {
         id: 2,
         title: "Develop website and add content",
-        completed: false
+        completed: false,
       },
       {
         id: 3,
         title: "Deploy to live server",
-        completed: false
+        completed: false,
       },
     ]
-   };
+  };
+
+  handleChange = id => {
+    this.setState(prevState => ({
+  // You can also remove this ^ parenthesis and use return statement
+  // return {
+      todos: prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo;
+      })
+  // } close the bracket
+  // v and remove that bracket  -- both methods work fine
+    }));
+  };
+
+  deleteTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
 
   render() {
     return (
       <div>
-        <TodosList todos={this.state.todos} />
+        <Header />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.deleteTodo}
+        />
       </div>
     );
   }
