ae0db749773362aedad1817fe22733fffc81375a
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-04-26 12:41:15 -0400

    Update TodoItem style and using destructuring

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
+
+import styles from'./TodoItem.module.css';
+
 class TodoItem extends Component {
   render() {
+    const { completed, id, title } = this.props.todo;
+
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    };
+
     return (
-      <li>
+      <li className={styles.item}>
         <input
-          type='checkbox'
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
         />
-        <button
-          onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
-        >
+        <button onClick={() => this.props.deleteTodoProps(id)}>
           Delete
         </button>
-        {this.props.todo.title}
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
       </li>
     );
   }
 }
 
 export default TodoItem;
+
+// When you use the Destructuring you need to create a const { nameStates1,
+//nameState2,... }, and remove this.props.< the parent state >
